581f622a49bd215e277c1a44df2d11cff07b59cc
agaghd <cover_story@163.com>
2019-11-05 21:32:49 +0800

    导入RecyclerView，创建数据对象类Task。

--- app/src/main/java/io/agaghd/daydayup/activities/MainActivity.kt
+++ app/src/main/java/io/agaghd/daydayup/activities/MainActivity.kt
@@ -1,12 +1,16 @@
-package io.agaghd.daydayup
+package io.agaghd.daydayup.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.agaghd.daydayup.R
+import io.agaghd.daydayup.pojo.Task
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var testTask = Task(2, "ss", 2, "ttt")
     }
 }
---
+++ app/src/main/java/io/agaghd/daydayup/pojo/Task.kt
@@ -0,0 +1,10 @@
+package io.agaghd.daydayup.pojo
+
+/**
+ * 任务对象
+ * id:id
+ * name：名称
+ * point：分数
+ * description:描述
+ */
+data class Task(var id: Long, var name: String, var point: Int, var description: String)
